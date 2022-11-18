cdf47f854454f7f433b1f8bf6d62175ca4022628
Jelson Rodrigues <jelsonrodrigues25@gmail.com>
2022-11-17 23:18:51 -0300

    EASTER EGG: Analog clock done

--- main.cpp
+++ main.cpp
@@ -5,35 +5,84 @@
 #include <algorithm>
 #include "utils.hpp"
 #include <cmath>
+#define M_PI 3.141592653589793238462643
 
 class Example : public olc::PixelGameEngine
 {
 public:
 	Example()
 	{
-		sAppName = "Example";
+		sAppName = "Clock";
 	}
 
 public:
 	int counter;
-	uint raio;
-
+	uint32_t raio;
+	olc::Pixel color = olc::Pixel(255, 165, 0);
 	bool draw(float fElapsedTime)
 	{
 		// called once per frame
-		Clear(olc::DARK_GREEN);
-		FillCircle(olc::vi2d(ScreenWidth() / 2, ScreenHeight() / 2), raio, olc::GREY);
-		DrawCircle(olc::vi2d(ScreenWidth() / 2, ScreenHeight() / 2), raio, olc::BLUE);
+		Clear(olc::Pixel(64, 128, 128, (counter % 128) + 127));
+
+		FillCircle(olc::vi2d(ScreenWidth() / 2, ScreenHeight() / 2), raio, olc::Pixel(155, 155, 155, 255));
+		DrawCircle(olc::vi2d(ScreenWidth() / 2, ScreenHeight() / 2), raio + 0, olc::CYAN);
+		DrawCircle(olc::vi2d(ScreenWidth() / 2, ScreenHeight() / 2), raio + 1, olc::BLUE);
+		DrawCircle(olc::vi2d(ScreenWidth() / 2, ScreenHeight() / 2), raio + 2, olc::BLACK);
+
+		// Seconds
+		uint8_t second = counter % 60;
 		DrawLine(
 			olc::vi2d(
 				ScreenWidth() / 2,
 				ScreenHeight() / 2
 			),
 			olc::vi2d(
-				ScreenWidth() / 2 + (raio * cos(counter * M_PI / 60.0)), 
-				ScreenHeight() / 2 + (raio * sin(counter * M_PI / 60.0))
+				ScreenWidth() / 2 + (raio * cos((((second) * 6.0) - 90.0) * M_PI / 180.0)), 
+				ScreenHeight() / 2 + (raio * sin((((second) * 6.0) - 90.0) * M_PI / 180.0))
 			),
-			olc::BLACK
+			color
+		);
+
+		// Minutes
+		uint8_t minute = (counter / 60) % 60;
+		DrawLine(
+			olc::vi2d(
+				ScreenWidth() / 2,
+				ScreenHeight() / 2
+			),
+			olc::vi2d(
+				ScreenWidth() / 2 + ((raio * 3/4) * cos((((minute) * 6.0) - 90.0) * M_PI / 180.0)), 
+				ScreenHeight() / 2 + ((raio * 3/4) * sin((((minute) * 6.0) - 90.0) * M_PI / 180.0))
+			),
+			olc::BACK
+		);
+
+		// Hours
+		#define GMT_OFFSET 3 
+		uint8_t hour = ((counter / (60 * 60) - GMT_OFFSET) % 12);
+		DrawLine(
+			olc::vi2d(
+				ScreenWidth() / 2,
+				ScreenHeight() / 2
+			),
+			olc::vi2d(
+				ScreenWidth() / 2 + ((raio / 2) * cos(((hour * 30) - 90) * M_PI / 180.0)), 
+				ScreenHeight() / 2 + ((raio / 2) * sin(((hour * 30) - 90) * M_PI / 180.0))
+			),
+			olc::DARK_MAGENTA
+		);
+
+
+		std::ostringstream stream;
+		stream << std::setw(2) << std::setfill('0') << (int) hour << ":" << std::setw(2) << std::setfill('0') << (int) minute << ":" << std::setw(2) << std::setfill('0') << (int) second;
+		std::string digital_hour = stream.str();
+
+		DrawString(
+			olc::vi2d(
+				ScreenWidth() / 2  - raio,
+				ScreenHeight() / 2 + raio + 10
+			),
+			digital_hour
 		);
 
 		counter = time(0);
@@ -184,7 +233,7 @@ int main()
 
 	{
 		Example app;
-		if (app.Construct(256, 256, 2, 2)){
+		if (app.Construct(640, 360, 2, 2)){
 			app.Start();
 		}
 	}
