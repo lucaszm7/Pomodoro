c9834438b1a09d65ffaf88e1ec89ec9108435bf6
Jelson Rodrigues <jelsonrodrigues25@gmail.com>
2022-12-01 11:54:04 -0300

    CHANGE: animation not linear

--- src/include/movingitem.hpp
+++ src/include/movingitem.hpp
@@ -33,8 +33,18 @@ public:
         else {
             double percentageOfFinalTime = (double) (fElapsedTime / timeToReachTargetPosition);
             olc::vi2d nextPosition = (finalPosition - startingPosition);
-            nextPosition.x *= percentageOfFinalTime;
-            nextPosition.y *= percentageOfFinalTime;
+            // nextPosition.x *= sin(percentageOfFinalTime*3.14 / 2.0) * cos(percentageOfFinalTime*3.14 - 3.14);
+            // nextPosition.y *= sin(percentageOfFinalTime*3.14 / 2.0) * cos(percentageOfFinalTime*3.14 - 3.14);
+            // nextPosition.x *= cos(percentageOfFinalTime*3.14 * 2.0 ) / 2.0 + 0.5;
+            // nextPosition.y *= sin(percentageOfFinalTime*3.14 / 2.0);
+            // nextPosition.x *= -1 * ((percentageOfFinalTime - 1) * (percentageOfFinalTime - 1) * (percentageOfFinalTime - 1) * (percentageOfFinalTime - 1)) + 1;
+            
+            // x moves by (x - 1) ^ 3 + 1
+            // y moves by (y) ^ 3
+            // this mens that the x value moves faster at the begining and slowly ant the end
+            // while the y moves slow at the begining of the animation and moves faster at the end
+            nextPosition.x *= ((percentageOfFinalTime - 1) * (percentageOfFinalTime - 1) * (percentageOfFinalTime - 1) + 1);
+            nextPosition.y *= percentageOfFinalTime*percentageOfFinalTime*percentageOfFinalTime;
             item.moveItem(nextPosition + startingPosition);
         }
         return true;
